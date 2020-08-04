9f0b34a4bedffdf4af280be8f17b927b705a5fd4
Patricia de Boer <pdeboer05@gmail.com>
2020-08-03 23:47:38 -0500

    Adding scss files

---
+++ client/index.js
@@ -0,0 +1,9 @@
+import './styles.css';
+import './index.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>Hello, world!</div>,
+  document.getElementById('main') // make sure this is the same as the id of the div in your index.html
+);
